ccee6274bcadd6638da6284a60b6c5fcfc80717e
slow-burner <jyjs0727@gmail.com>
2026-07-25 22:51:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import streamlit as st
+import math
+
 st.write("안녕하세요")
 print("안녕하세요.")
 i = 0
 #for 문을 활용해 "안녕하세요" 3회 출력
+
 data = 3
 for i in range(data):
   st.write("안녕하세요")
   
+st.write(ceil(33.333333333))
